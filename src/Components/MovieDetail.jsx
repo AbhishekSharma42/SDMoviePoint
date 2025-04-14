82d8bda85286d4c2674f32c6b538d67189b2b6a0
Abhishek sharma <ak9969489@gamil.com>
2025-04-14 11:22:35 +0530

    show movie detail

--- src/Components/MovieDetail.jsx
+++ src/Components/MovieDetail.jsx
@@ -16,12 +16,15 @@ const MovieDetail = () => {
         const parser = new DOMParser();
         const doc = parser?.parseFromString(proxyData.contents, 'text/html');
         const movieElements = doc?.querySelector('.entry-content');
+        const MovieLink = doc?.querySelector('.entry-content tbody tr td form');
 
-        console.log(movieElements);
+        console.log(MovieLink)
+
+        // 9690873758
 
         const movieData = {
             img: movieElements?.querySelector('p strong img')?.src,
-            title: movieElements?.querySelector('div h1 span strong').innerHTML,
+            title: doc?.querySelector('.entry-title')?.textContent,
         }
         setMovie(movieData)
     }
@@ -31,16 +34,21 @@ const MovieDetail = () => {
 
     return (
         <>
-            <div className="flex flex-col justify-center items-center mt-10">
-                <h1 className="text-3xl font-bold">Movie Detail</h1>
+            <div className="flex md:text-2xl flex-col justify-center items-center mt-5">
+                <h2 className="text-md font-semibold mt-2 text-center px-3">{getMovie?.title}</h2>
+                <h1 className="text-xl font-bold pt-2 underline">Movie Detail</h1>
                 <div className="flex flex-col items-center mt-5">
                     <img src={`${getMovie?.img}`} alt="Movie Poster" className="w-72 h-96 object-cover rounded-lg shadow-md" />
                     <span>overview</span>
-                    <h2 className="text-xl font-semibold mt-2">{getMovie?.title}</h2>
+                    <h2 className="text-md font-semibold mt-2 text-center px-3">{getMovie?.title}</h2>
                     <p className="text-gray-600 mt-1">Release Date: 2023-10-01</p>
                     <p className="text-gray-600 mt-1">Genre: Action, Adventure</p>
                     <p className="text-gray-600 mt-1">Rating: 8.5/10</p>
                     <p className="text-gray-600 mt-1">Description: This is a brief description of the movie.</p>
+                    <div className='w-full mt-4 mx-auto flex justify-center items-center gap-5'>
+                        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Download</button>
+                        <button className="mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Download</button>
+                    </div>
                 </div>
             </div>
         </>
