02755d7aa158825a37b2e2abf90435c1f708156a
Abhishek Sharma <89927608+AbhishekSharma42@users.noreply.github.com>
2025-06-05 21:28:57 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
         try {
             setSearchMovie(getSearch);
             const corsProxy = 'https://api.allorigins.win/get?url=';
-            const originalUrl = `https://sdmoviespoint.diy/?s=${getSearch}`;
+            const originalUrl = `https://sdmoviespoint.media/?s=${getSearch}`;
             const url = corsProxy + encodeURIComponent(originalUrl);
             const response = await fetch(url);
             const proxyData = await response?.json();
